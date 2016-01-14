32181446ee992af3f4d20f530869acd50885d102
Yerko Palma <yerko.palma@usach.cl>
2016-01-14 17:02:22 +0000

    added first json response
    After POST login, now the result is rendered as json and no redirect is performed

--- app/routes.js
+++ app/routes.js
@@ -32,11 +32,11 @@ module.exports = function(app, passport) {
         });
 
         // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+        app.post('/login', 
+            passport.authenticate('local-login'),
+            function(req, res) {
+                res.json(req.user);
+            });
 
         // SIGNUP =================================
         // show the signup form
