6ca01c926d6546535207d5eceb8e950afc69d43f
YerkoPalma <yerko.palma@usach.cl>
2016-01-15 00:40:48 -0300

    Added first working tests
    Fixed wrong Mongoose connection

--- test/routes.js
+++ test/routes.js
@@ -1,6 +1,6 @@
-var should = require('should'); 
+var should = require('should');
 var assert = require('assert');
-var request = require('supertest');  
+var request = require('supertest');
 var mongoose = require('mongoose');
 //var winston = require('winston');
 var config = require('../config/database.js');
@@ -8,24 +8,24 @@ var app = require('../server.js');
 var agent = request.agent(app);
 
 describe('Routing', function() {
-  
+
   before(function(done) {
-    mongoose.connect(config.url);	//testDB is empty, so connect to passport db for now
+    mongoose.createConnection(config.url);	//testDB is empty, so connect to passport db for now
     done();
   });
-  
+
   describe('login', function(){
     it('should get JSON data after login', function(done){
-      
+
       var userdata = {
-        email: '',
-        password: ''
+        email: 'yerko.palma@usach.cl',
+        password: 'dezodio'
       };
-      
+
       agent
         .post('/login')
         .send(userdata)
-        //.expect('Content-Type', /json/)
+        .expect('Content-Type', /json/)
 		    .expect(200) //Status code
 		    .end(function(err, res){
 		      if (err) {
@@ -36,8 +36,8 @@ describe('Routing', function() {
     			res.body.local.email.should.equal(userdata.email);
     			done();
 		    });
-      
+
     })
   });
-  
+
 });
