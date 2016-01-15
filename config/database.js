d2c106a3dac853bf501a3cfda9d158019c9f1217
Yerko Palma <yerko.palma@usach.cl>
2016-01-15 17:48:36 +0000

    Added special configuration for CI
    By using special mongoDb Env vars provided by Wercker

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@
 module.exports = {
 
     'url' : 'mongodb://localhost/passport', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'test' : 'mongodb://localhost/test'
+    'test' : 'mongodb://localhost/test',
+    'wercker': 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT + '/test'
 
 };
