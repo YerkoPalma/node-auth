f79c88f657863e53a6cd4699b1588cbcee648a0c
YerkoPalma <yerko.palma@usach.cl>
2016-01-27 22:43:28 -0300

    changed signup route

--- app/routes.js
+++ app/routes.js
@@ -32,7 +32,7 @@ module.exports = function(app, passport) {
         });
 
         // process the login form
-        app.post('/login', 
+        app.post('/login',
             passport.authenticate('local-login'),
             function(req, res) {
                 res.json(req.user);
@@ -45,11 +45,12 @@ module.exports = function(app, passport) {
         });
 
         // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+        app.post('/signup',
+            passport.authenticate('local-signup'),
+            function(req, res) {
+                res.json(req.user);
+            }
+          );
 
     // facebook -------------------------------
 
