718866b15cd90eaed8a4d24994f13076785b87df
Yerko Palma <yerko.palma@usach.cl>
2016-02-02 14:05:00 +0000

    fixed openshift db uri

--- config/database.js
+++ config/database.js
@@ -4,5 +4,5 @@ module.exports = {
     'url' : 'mongodb://localhost/passport', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'test' : 'mongodb://localhost/test',
     'wercker': 'mongodb://172.17.0.6:27017/test',
-    'openshift': 'admin:rmPuq6j_f2hA@mongodb://127.2.188.130:27017/nodeauth'
+    'openshift': 'mongodb://admin:rmPuq6j_f2hA@127.2.188.130:27017/nodeauth'
 };
