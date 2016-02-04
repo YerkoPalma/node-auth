59a0f462d155edb643aff13dea59cdabe2b71f5f
Yerko Palma <yerko.palma@usach.cl>
2016-02-04 02:50:57 +0000

    added database config

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://localhost/passport', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'test' : 'mongodb://localhost/test',
+    'wercker': 'mongodb://172.17.0.6:27017/test',
+    'openshift': 'mongodb://admin:rmPuq6j_f2hA@127.2.188.130:27017/nodeauth'
+};
